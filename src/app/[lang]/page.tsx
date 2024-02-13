8503f1eb8fba940ff2f9dbee7a56946e40992862
kateryna-len <lenkaterynna057@gmail.com>
2024-02-13 18:07:59 +0200

    added InternationalizationNext

---
+++ src/app/[lang]/page.tsx
@@ -0,0 +1,106 @@
+import Image from 'next/image'
+import { Locale, getDictionary } from './dictionaries'
+import styles from './page.module.css'
+import SwitchLang from '@/components/SwitchLang/SwitchLang'
+
+type Props = {
+  params: {
+    lang: Locale
+  }
+}
+
+export default async function Home({ params: { lang } }: Props) {
+  const intl = await getDictionary(lang)
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          {intl.get_started}&nbsp;
+          <code className={styles.code}>src/app/page.tsx</code>
+        </p>
+        <SwitchLang />
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>{intl.doc}</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>{intl.learn}</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>{intl.template}</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            {intl.deploy}
+          </p>
+        </a>
+      </div>
+    </main>
+  )
+}
